0bebe8e1468aa85564b22e5e20a6c7dfedce7d9f
paul.puech2 <paul.puech2@ext.soprasteria.com>
2024-11-01 21:43:02 +0100

    🌱 chore: commit de sauvegarde

--- app/build.py
+++ app/build.py
@@ -26,27 +26,36 @@ def item(
         data = load_data("data/data.json")
         items = data.get("items", [])
         recipes = data.get("recipes", [])
+        resources = data.get("resources", [])
+        fluids = data.get("fluids", [])
 
-        # Check if the item queried exists in the items array based on the key_name
+        # Check if the initial item queried exists in the items array based on the key_name
         item_exists = any(i["key_name"] == query for i in items)
         if not item_exists:
             console.print(f"[red]Item '{query}' not found.[/red]")
-            return
+            raise typer.Exit(code=1)
 
-        # Get all the recipes that have the queried item in their products
-        matching_recipes = [
-            recipe
-            for recipe in recipes
-            if any(p[0] == query for p in recipe["products"])
-        ]
-        if not matching_recipes:
-            console.print(f"[red]No recipe found for item '{query}'.[/red]")
-            return
-
+        # Get the available recipe for item
+        matching_recipes = get_recipes_for_item(recipes, query)
         display_recipes(matching_recipes, "Matching recipes")
 
-        ## TOOD
-        ## Maintenant faut demander à choisir la recette qu'on veut
+        # Ask the user to choose a recipe
+        recipe = choose_recipe(matching_recipes)
+        display_recipes([recipe], "recette choisie")
+
+        # Check if the recipe has any ingredients that are neither resources nor fluids
+        complex_ingredients = check_ingredients(recipe, resources, fluids)
+        if len(complex_ingredients) >= 1:
+            console.print(
+                f"[red]The following ingredients have to be build: {', '.join(complex_ingredients)}.[/red]"
+            )
+            return
+        else:
+            print("fin de build")
+
+        # FIN
+
+        # Voir pour sauvegarder le build final si c'est utile
 
     except FileNotFoundError:
         console.print(
@@ -54,3 +63,44 @@ def item(
         )
     except json.JSONDecodeError:
         console.print("[red]Error decoding JSON data.[/red]")
+
+
+def choose_recipe(matching_recipes):
+    console.print("Choose a recipe to use:")
+    for i, recipe in enumerate(matching_recipes):
+        console.print(f"[{i+1}] {recipe['key_name']}")
+    choice = typer.prompt("Enter the number of the recipe you want to use")
+    try:
+        choice = int(choice)
+        if choice < 1 or choice > len(matching_recipes):
+            console.print("[red]Invalid choice.[/red]")
+            raise typer.Exit(code=1)
+        return matching_recipes[choice - 1]
+    except ValueError:
+        console.print("[red]Invalid choice.[/red]")
+        raise typer.Exit(code=1)
+
+
+def check_ingredients(recipe, resources, fluids):
+    complex_ingredients = []
+    for ingredient in recipe["ingredients"]:
+        ingredient_key_name = ingredient[0]
+        if not any(r["key_name"] == ingredient_key_name for r in resources) and not any(
+            f["key_name"] == ingredient_key_name for f in fluids
+        ):
+            complex_ingredients.append(ingredient_key_name)
+    return complex_ingredients
+
+
+def get_recipes_for_item(recipes, query_item):
+    # Get all the recipes that have the queried item in their products
+    matching_recipes = [
+        recipe
+        for recipe in recipes
+        if any(p[0] == query_item for p in recipe["products"])
+    ]
+    if not matching_recipes:
+        console.print(f"[red]No recipe found for item '{query_item}'.[/red]")
+        raise typer.Exit(code=1)
+
+    return matching_recipes
